825fead9108c0d1ea93ae706a3d5d70034be2b3e
MATHEESHA <95665347+MATHEESHA2@users.noreply.github.com>
2025-12-12 13:44:56 +0530

    `DL OUTPUTS`

---
+++ shared_data.py
@@ -0,0 +1,11 @@
+# shared_data.py
+screen_width = 0
+screen_height = 0
+
+screen_center_x = 0
+screen_center_y = 0
+
+object_center_x = None
+object_center_y = None
+object_class = None
+object_track_id = None
